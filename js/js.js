d093396f1fe4c5b2b3f48e8e58dc7b1b031e4c9e
lele_zhang <290367450@qq.com>
2015-08-07 14:21:22 +0800

    三国

---
+++ js/js.js
@@ -0,0 +1,86 @@
+﻿// JavaScript Document
+	   
+	   
+	   
+	   //--弹框----
+		var plusBankBg,showBox;
+		function showContent_0(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_0(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		function showContent_1(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox1');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_1(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox1');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		function showContent_2(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox2');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_2(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox2');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		function showContent_3(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox3');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_3(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox3');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		function showContent_4(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox4');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_4(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox4');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		function showContent_5(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox5');
+			plusBankBg.show();
+			showBox.slideDown();
+		}
+		function closeConten_5(){
+			plusBankBg=$('.plusBankBg');
+			showBox=$('.plusBox5');
+			plusBankBg.hide();
+			showBox.slideUp();
+		}
+		
+		
+		//--弹框 end----
